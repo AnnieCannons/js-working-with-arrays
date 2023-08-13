d25b9ed47a43682149bca0209e6a7fedce838b0b
shan wolff <shanwolff182@gmail.com>
2023-08-13 15:08:25 -0700

    Exercises 1-5 complete

--- scripts.js
+++ scripts.js
@@ -11,28 +11,41 @@
 /********************** Exercise 2 **********************/
 // Create an array of books to be sold in an online store. 
 
+let booksToBeSold = ["Sekret Machines: Book 1: Chasing Shadows", "Strange Times", "A Planet of Blood and Ice", "Poet Anderson"];
 
+console.log(booksToBeSold);
 
 /********************** Exercise 3 **********************/
+
 const shopping = ["bread", "milk", "cheese", "hummus", "noodles"];
 
 // For the above array, log the string "bread" to the console.
-
+console.log(shopping[0])
 
 // After the following code is run, what will happen? Console log the new array.
 shopping[0] = "rice";
+console.log (shopping[0]);
 
 
 // Try changing the value "cheese" to "vegetables" using an index.
+shopping[2];
+console.log(shopping[2]);
+
+shopping[2] = "vegetables";
+console.log(shopping[2]);
 
 // Log the value at index 2. Note that it is NOT the second value! Why is that?
+console.log(shopping[2]);
+"Because the beginning index value is 0, at value 1"
 
 // Log the last value WITHOUT using a set number. That is, do NOT write: `shopping[4]`. You can use the `.length` to figure out what the last index is!
+console.log(shopping.length);
 
 // Now write the same code in the square brackets, using the `.length` property to calculate the last index, only with the `gems` array below instead of `shopping`. The solution should give you the last value in ANY array!
 
 const gems = ["ruby", "saphhire", "pearl", "steven", "amethyst", "garnet", "lapis lazuli", "peridot"]
 
+console.log(gems.length);
 
 /********************** Exercise 4 **********************/
 
@@ -40,19 +53,31 @@ const cities = ["Oakland", "New York"];
 
 
 // Use an array method to add Memphis to the END of the array.
+cities.push('Memphis');
+console.log(cities);
 
 // Use an array method to add Phoenix to the END of the array.
+cities.push('Pheonix');
+console.log(cities);
 
 // Use an array method to add Atlanta to the START of the array.
+cities.unshift('Atlanta');
+console.log(cities);
 
 // Use an array method to add Chicago and Denver to the END of the array with one method call.
+cities.push('Chicago', 'Denver');
+console.log(cities);
 
 // Use an array method to remove the FIRST element of the array.
+cities.shift();
+console.log(cities);
 
 // Use an array method to remove the LAST element of the array.
+cities.pop();
+console.log(cities);
 
 // Log the final array to the console. It should look like this: ["Oakland", "New York", "Memphis", "Phoenix", "Chicago"]
-
+console.log(cities);
 
 /********************** Exercise 5 **********************/
 
@@ -60,10 +85,13 @@ const colors = ["red", "green", "blue", "yellow"];
 
 // Use the slice method on the above array to produce the following array: ["red", "green", "blue"]
 
+console.log(colors.slice(0,3));
+
 // Now use the slice method to produce the following array: ["green", "blue", "yellow"]
+console.log(colors.slice(1, 4));
 
 // Next use the slice method to produce the following array: ["red", "green"]
-
+console.log(colors.slice(0,2));
 
 
 /********************** Exercise 6 **********************/
